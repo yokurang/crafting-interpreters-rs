0c3ca2c9286d56959ba56d4692fe89ebbdc51cde
Alan Matthew <alan.matthew.yk@gmail.com>
2025-07-04 13:18:38 +0800

    fixed main

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,20 @@
 use std::env;
 use crafting_interpreters::runner::{run_file, run_prompt};
-use crafting_interpreters::lexer::{Token, TokenType};
-use crafting_interpreters::expr::Expr;
-use crafting_interpreters::{define_ast, Literal};
 
-// fn main() {
-// 
-//     let expression = Expr::Binary {
-//         left: Box::new(Expr::Unary {
-//             operator: Token {
-//                 token_type: TokenType::Minus,
-//                 lexeme: "-".to_string(),
-//                 literal: Literal::Nil,
-//                 line: 1,
-//             },
-//             right: Box::new(Expr::Literal {
-//                 value: Literal::Number(123.0),
-//             }),
-//         }),
-//         operator: Token {
-//             token_type: TokenType::Star,
-//             lexeme: "*".to_string(),
-//             literal: Literal::Nil,
-//             line: 1,
-//         },
-//         right: Box::new(Expr::Grouping {
-//             expression: Box::new(Expr::Literal {
-//                 value: Literal::Number(45.67),
-//             }),
-//         }),
-//     };
-// 
-//     let mut printer = AstPrinter;
-//     println!("{}", printer.print(&expression));
-// }
-
-// pub fn main() -> () {
-//     let args: Vec<String> = env::args().collect();
-//     // args always includes the program name in args[0]
-//     match args.len() {
-//         1 => {
-//             run_prompt();
-//         }
-//         2 => {
-//             run_file(&args[1]);
-//         }
-//         _ => {
-//             println!("Usage: jlox [script]");
-//             std::process::exit(64);
-//         }
-//     }
-// }
-
-fn main() -> std::io::Result<()> {
-    let output_dir = "./generated";
-
-    define_ast(
-        output_dir,
-        "Expr",
-        vec![
-            "Binary   : Box<Expr> left, Token operator, Box<Expr> right",
-            "Grouping : Box<Expr> expression",
-            "Literal  : LiteralValue value",
-            "Unary    : Token operator, Box<Expr> right",
-            "Variable : Token name",
-        ],
-    )?;
-
-    define_ast(
-        output_dir,
-        "Stmt",
-        vec![
-            "Expression : Box<Expr> expression",
-            "Print      : Box<Expr> expression",
-            "Var        : Token name, Box<Expr> initializer",
-        ],
-    )?;
-
+pub fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    // args always includes the program name in args[0]
+    match args.len() {
+        1 => {
+            run_prompt();
+        }
+        2 => {
+            run_file(&args[1]);
+        }
+        _ => {
+            println!("Usage: jlox [script]");
+            std::process::exit(64);
+        }
+    }
     Ok(())
 }
